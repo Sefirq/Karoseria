137ded0f1005f8b6f3af966db1f48f0ced9bf7ac
Adam Pioterek <adam.pioterek@protonmail.ch>
2016-11-15 18:36:19 +0100

    Planes & OLX

---
+++ images.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from imread import imread
+from utilities import edgy, edgy_color
+
+images = ["./img/samolot" + str(i // 10) + str(i % 10) + ".jpg" for i in range(0, 21)]
+
+basic_images = ["./img/samolot" + str(i // 10) + str(i % 10) + ".jpg" for i in (0, 1, 3, 4, 5, 7)]
+advanced_images = ["./img/samolot" + str(i // 10) + str(i % 10) + ".jpg" for i in (0, 1, 2, 3, 5, 7, 8, 9, 10, 11, 12,
+                                                                                   13, 14, 15, 16, 17, 18, 19)]
+
+fig = plt.figure(figsize=(60, 40))
+fig.set_facecolor('black')
+plt.subplots_adjust(left=0.0, bottom=0.0, right=1.0, top=1.0,
+                    wspace=0.0, hspace=0.0)
+for i in range(6):
+    print("basic " + str(i + 1) + "/6")
+    a = fig.add_subplot(2, 3, i + 1)
+    image = edgy(basic_images[i])
+    plt.imshow(image, cmap='gray')
+    plt.axis('off')
+plt.savefig('basic.pdf', facecolor=fig.get_facecolor())
+
+fig2 = plt.figure(figsize=(60, 60))
+
+for i in range(18):
+    print("advanced " + str(i + 1) + "/18")
+    original = imread(advanced_images[i], as_grey=False)
+    b = fig2.add_subplot(7, 3, i + 1)
+    image = edgy_color(advanced_images[i])
+    plt.imshow(original)
+    plt.imshow(image)
+    plt.axis('off')
+plt.savefig('advanced.pdf')
---
+++ utilities.py
@@ -0,0 +1,89 @@
+import numpy as np
+from skimage.color import rgb2hsv
+from imread import imread
+from scipy import ndimage, random
+from skimage.filters import gaussian, sobel_h, sobel_v
+from skimage.segmentation import find_boundaries
+from skimage.morphology import dilation
+from skimage.draw import circle
+
+
+def discretize(image):
+    threshold = 20
+    black = image < threshold
+    white = image >= threshold
+    image[black] = 0
+    image[white] = 255
+    return image
+
+
+def edgy(image_name):
+    image = imread(image_name, as_grey=True)
+    image = gaussian(image, sigma=.97)
+    image = custom_sobel(image)
+    image = discretize(image)
+    return image
+
+
+def custom_sobel(image):
+    edge_horizont = sobel_h(image)
+    edge_vertical = sobel_v(image)
+    magnitude = np.hypot(edge_horizont, edge_vertical)
+    return magnitude
+
+
+def edgy_color(image_name):
+    image = imread(image_name, as_grey=False)
+
+    image = rgb2hsv(image)
+    image = image[..., 2]
+
+    objects, objects_count = ndimage.label(image < .5)
+
+    sizes = ndimage.sum(image, objects, range(objects_count + 1))
+    mask_size = sizes < 35
+    remove_pixel = mask_size[objects]
+    objects[remove_pixel] = 0
+    labels = np.unique(objects)
+    objects = np.searchsorted(labels, objects)
+
+    contours = find_boundaries(objects)
+    objects[contours == 0] = 0
+    objects = dilation(objects)
+    objects = dilation(objects)
+
+    colours = []
+    for _ in range(objects_count + 1):
+        colours.append([random.randint(256) / 255 for _ in range(3)] + [1])
+    colours[0][3] = 0
+
+    mask = np.zeros_like(objects, dtype=np.float64)
+    x, y = objects.shape
+    mask.resize(x, y, 4)
+    for r, row in enumerate(objects):
+        for c, element in enumerate(row):
+            mask[r, c] = colours[element]
+
+    centroids = find_centroids(objects)
+    for c in centroids:
+        rr, cc = circle(c[0], c[1], int(x / 100))
+        mask[rr, cc] = 1
+
+    return mask
+
+
+def find_centroids(labels):
+    centroids_all = {}
+    for i, row in enumerate(labels):
+        for j, element in enumerate(row):
+            if element not in centroids_all:
+                centroids_all[element] = {'x': 0, 'y': 0, 'n': 0}
+            centroids_all[element]['x'] += i
+            centroids_all[element]['y'] += j
+            centroids_all[element]['n'] += 1
+
+    centroids = []
+    for key, value in centroids_all.items():
+        if key != 0:
+            centroids.append((value['x'] / value['n'], value['y'] / value['n']))
+    return centroids
